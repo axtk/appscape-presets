a6344deb449395dece68786c626f6a5b48c3a1d0
axtk <axtkx@inbox.ru>
2024-12-25 12:06:02 +0300

    add useRouteLinks test to routescape-test

--- routescape-test/src/entries/main/ui/Intro/index.tsx
+++ routescape-test/src/entries/main/ui/Intro/index.tsx
@@ -1,11 +1,20 @@
+import {useRef} from 'react';
+import {useRouteLinks} from 'routescape';
 import {Counter} from '../Counter';
 
-export const Intro = () => (
-    <main>
-        <h1>Intro</h1>
-        <p>This is demo content. Lorem ipsum dolor, quam velit, tincidunt vitae suscipit nullam.</p>
-        <Counter/>
-    </main>
-);
+export const Intro = () => {
+    let containerRef = useRef<HTMLElement | null>(null);
+
+    useRouteLinks(containerRef, 'a');
+
+    return (
+        <main ref={containerRef}>
+            <h1>Intro</h1>
+            <p>This is demo content. Lorem ipsum dolor, quam velit, tincidunt vitae suscipit nullam.</p>
+            <p>This <a href="/about">HTML link</a> is converted to a route link.</p>
+            <Counter/>
+        </main>
+    );
+}
 
 export default Intro;
